97b3b66b78b54d1c1a1693d312fd57f44c7b4bff
alenanish <61657990+alenanish@users.noreply.github.com>
2023-06-16 08:50:44 +0300

    Add files via upload

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,12 +1,16 @@
+/*
+# Copyright (c) https://github.com/alenanish/devops_lab1 . All rights reserved.
+*/
 #pragma once
+#include <string>
 
 struct ListNode {
     int val;
     ListNode* next;
     ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
-    ListNode(ListNode* head) : val(head->val), next(head->next) {}
+    explicit ListNode(ListNode* head) : val(head->val), next(head->next) {}
 };
 
 void printList(ListNode* head);
@@ -16,12 +20,13 @@ std::string getResult(ListNode* head);
 ListNode* createList(int n, int d);
 
 void deleteList(ListNode* head);
+
 std::string mergeTwoLists(int n, int m);
 
 std::string reverseList(int n);
 
 int countNodes(int n);
-
 int countNodes(ListNode* head);
 
 std::string rotateRight(int n, int k);
+
--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+/*
+# Copyright (c) https://github.com/alenanish/devops_lab1 . All rights reserved.
+*/
+#include <iostrea>
 #include <string>
 #include "LinkedList.hpp"
 
-
-
-int main(){
-  std::cout << mergeTwoLists(5, 5) << std::endl;
+int main() {
+    std::cout << mergeTwoLists(5, 5) << std::endl;
 }
